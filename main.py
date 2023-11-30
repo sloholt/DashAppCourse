edac70465c69d8d1cc1ef0b338751ac718146a82
sloholt <sloaneholtby@gmail.com>
2023-11-30 14:31:35 -0500

    Updated main.py for Dash 20 example

--- main.py
+++ main.py
@@ -1,5 +1,45 @@
-import dash 
+from dash import Dash, html, dash_table, dcc, callback, Output, Input 
 import dash_bootstrap_components as dbc
-import pandas
+import pandas as pd
+import plotly.express as px 
+import dash_ag_grid as dag 
 
-print("hello world")
+#df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/Dash-Course/makeup-shades/shades.csv", sep=',')
+
+#testing Dash in 20 example with VSCode
+# Incorporate data
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+
+# Plotly graphs
+fig = px.histogram(df, x='continent', y='pop', histfunc='avg')
+
+# Initialize the app
+app = Dash(__name__)
+
+# App layout
+app.layout = html.Div([
+    html.Div(children='My First App with Data, Graph, and Controls'),
+    html.Hr(),
+    dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'], value='lifeExp', id='column-options'),
+    dag.AgGrid(
+        id="grid",
+        rowData=df.to_dict("records"),
+        columnDefs=[{"field": i} for i in df.columns],
+    ),
+    dcc.Graph(figure=fig, id='graph1')
+])
+
+# Add controls to build the interaction
+@callback(
+    Output(component_id='graph1', component_property='figure'),
+    Input(component_id='column-options', component_property='value')
+)
+def update_graph(col_chosen):
+    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
+    return fig
+
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
+
